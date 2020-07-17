f4cd486007fb1eac4d0e4a2603e20b67622aba8d
Neelabh Mam <neelabh.mam@gmail.com>
2020-07-18 00:09:16 +0530

    initial commit

--- MoCap.cpp
+++ MoCap.cpp
@@ -12,10 +12,10 @@
 #include <Common.hpp>
 #include <Source.hpp>
 #include <Tracker.hpp>
+#include <Person.hpp>
 
-TrackingManager tm;
-
-uint64_t people = 0;
+std::set<Person *> people;
+Person* ProcessContour(const std::vector<Point>& contour);
 
 int main(int argc, char *argv[])
 {
@@ -40,8 +40,7 @@ int main(int argc, char *argv[])
       if(s.HasEnded())
       {
         s.Rewind();
-        tm.ClearAllContexts();
-        people = 0;
+        people.clear();
         continue;
       }
       else
@@ -71,56 +70,70 @@ int main(int argc, char *argv[])
 
     cv::dilate(thresh, dilate, cv::Mat(), cv::Point(-1, -1), 2);
 
-    //Find the contours. Use the contourOutput Mat so the original image doesn't get overwritten
-
-    // cv::Mat contourOutput = dilate.clone();
-
-    std::vector<std::vector<cv::Point>> contours;
+    std::vector<
+      std::vector<cv::Point>
+    > contours, filtered;
 
     cv::findContours(dilate, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
     for (size_t i = 0; i < contours.size(); i++) 
     {
       auto bb = cv::boundingRect(contours[i]);
-      
+
       if (cv::contourArea(contours[i]) < 10000) continue;
       if (bb.width > bb.height) continue;
 
-      /*
-       * skip this contour if:
-       * it is inside any other bb
-       * A is less than the threshold
-       */
+      bool skip = false;
+
       for (size_t j = 0; (j < contours.size() && (i != j)); j++)
       {
         if (IsRectInsideRect(bb, cv::boundingRect(contours[j])))
         {
+          skip = true;
           break;
         }
-        else
-        {
-          auto rv = cv::matchShapes(contours[i], contours[j], 1, 0.0);
-          
-          if (rv < 0.3) std::cout << rv << "\n";
+      }
 
-          if (1)
-          {
-            people++;
-            cv::rectangle(frame, bb, cv::Scalar(255, 0, 0 ), 1, 1);            
-          }
-        }
+      if (!skip)
+      {
+        ProcessContour(contours[i]);
+        cv::rectangle(frame, bb, cv::Scalar(255, 0, 0 ), 1, 1);            
       }
     }
 
-    /*
-     * update all active trackers first
-     */
-
-    cv::putText(frame, std::to_string(people), cv::Point(5, 20), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0, 0, 255), 1);
+    cv::putText(frame, std::to_string(people.size()), cv::Point(5, 20), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0, 0, 255), 1);
 
 	  cv::imshow("Motion Capture", frame);
+
     cv::imshow("Mask", delta);
 
     if (!s.HandleUserInput()) break;
   }   
 }
+
+    Person* ProcessContour(const std::vector<Point>& contour) 
+    {
+        time_p now = high_resolution_clock::now();
+        Person* person = NULL;
+    
+        for (std::set<Person*>::iterator it = people.begin(); it != people.end(); ++it) {
+            if ((*it)->hasSimilarContour(contour)) {
+                person = *it; 
+                person->lastSeen = now;
+                person->update(contour);
+                //countIfPersonIsCrossingTheRefLine(person);
+                break;
+            }
+        }
+
+        if (person == NULL) {
+            person = new Person(contour);
+            person->firstSeen = now;
+            person->lastSeen = now;
+            people.insert(person);
+        }
+
+        //lastFrameWherePersonWasSeen[person] = frameNumber;
+
+        return person;
+    }
